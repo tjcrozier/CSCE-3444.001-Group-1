6eadc8270c2a25c9fbbb33919afe8ba025be5358
MoTWilliams <MoTaylorWilliams@gmail.com>
2025-04-04 01:25:37 -0500

    Edited summarizeFunction() to utilize the new Selection class

--- summaryGenerator.js
+++ summaryGenerator.js
@@ -1,45 +1,31 @@
 const vscode = require('vscode'); // VSCode API
-const { detectCurrentFunction } = require('./codeParser');
+const { Selection } = require('./codeParser');
 const { speakMessage } = require('./speechHandler')
 
-function summarizeFunction() {
-    const editor = vscode.window.activeTextEditor;
+function summarizeFunction(editor) {
+    // Attempt to retrieve the current function
+    const currentFunction = new Selection('function');
+    currentFunction.detectCurrentBlock(editor);
 
-    // Ensure there is an editor open
-    if(!editor) {
-        console.error("No editor open.");
-        return;
-    }
-    
-    // Ensure the current document is in Python
-    if (editor.document.languageId !== 'python') {
-        console.error("EchoCode can only generate docstrings for Python code.");
-        return;
-    }
-
-    // Attempt to retrieve current function
-    const currentFunction = detectCurrentFunction(editor);
-
-    // Cannot insert docstring if the cursor is not in a function
-    if (!currentFunction.cursorInFunction) {
+    // Nothing to summarize if the cursor is not in a function
+    if (!currentFunction.cursorInSelection) {
         console.error("Cursor is not in a function. No summary generated.\n");
         speakMessage("Cursor is not inside a function.")
         return;
     }
 
     // Otherwise, print the text of the current function to the console (for now)
-    const functionText = currentFunction.functionText;
+    const functionText = currentFunction.text;
     console.error("Will generate summary for the following function:")
     console.log("---FUNCTION TEXT---\n", functionText, "\n---END FUNCTION TEXT---\n");
 
-    generateSummary(currentFunction).then(summary => {
+    generateSummary(currentFunction.name).then(summary => {
         speakMessage("Generating summary for " + summary);
     });
 }
 
-async function generateSummary(block) {
-
-    return block.definition.text;
+async function generateSummary(blockName) {
+   return blockName; // Dummy output
 }
 
 module.exports = { summarizeFunction };
