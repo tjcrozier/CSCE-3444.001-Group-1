1b9ac70a12ef0b864bee2af053f7d79799fafea2
Oluwaniyi, Funminiyi <funminiyioluwaniyi@my.unt.edu>
2025-04-18 18:20:33 -0500

    Implement stopSpeech functionality in speechHandler and ensure ongoing speech is halted before speaking a new message.

--- speechHandler.js
+++ speechHandler.js
@@ -1,14 +1,33 @@
 const say = require('say');
 
+let isSpeaking = false;
+
+function stopSpeech() {
+    if (isSpeaking) {
+        say.stop();
+        isSpeaking = false;
+        return true;
+    }
+    return false;
+}
+
 function speakMessage(message) {
     return new Promise((resolve) => {
+        // Stop any ongoing speech before starting new one
+        stopSpeech();
+        
+        isSpeaking = true;
         say.speak(message, undefined, 0.8, (err) => {
             if (err) {
                 console.error('Speech Error:', err);
             }
+            isSpeaking = false;
             resolve();
         });
     });
 }
 
-module.exports = { speakMessage };
+module.exports = {
+    speakMessage,
+    stopSpeech
+};
